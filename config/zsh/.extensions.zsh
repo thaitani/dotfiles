938afc7c5f6a88af267d5177b42de16c68713cb6
thaitani <42672817+thaitani@users.noreply.github.com>
2026-04-07 13:57:07 +0900

    refactor(zsh): zinitの遅延ロードを廃止して初期状態を即時化

--- config/zsh/.extensions.zsh
+++ config/zsh/.extensions.zsh
@@ -14,73 +14,56 @@ mkcd() {
 }
 
 ### asdf ###
-__asdf_atload() {
-  export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
-  export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
-  source $ASDF_DIR/asdf.sh
-}
-zinit wait lucid light-mode for \
-  atpull'asdf plugin update --all' \
-  atload'__asdf_atload' \
+export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
+export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
+zinit lucid light-mode for \
   @'asdf-vm/asdf'
+source "$ASDF_DIR/asdf.sh"
 
 ### direnv ###
-__direnv_atload() {
-  eval "$(direnv hook zsh)"
-}
-zinit wait lucid light-mode as'program' from'gh-r' for \
+zinit lucid light-mode as'program' from'gh-r' for \
   mv'direnv* -> direnv' \
-  atload'__direnv_atload' \
   @'direnv/direnv'
+eval "$(direnv hook zsh)"
 
 ### yq ###
-zinit wait lucid light-mode as'program' from'gh-r' for \
+zinit lucid light-mode as'program' from'gh-r' for \
   mv'yq* -> yq' \
   atclone'./yq shell-completion zsh >_yq' atpull'%atclone' \
   @'mikefarah/yq'
 
 ### bat ###
-__bat_atload() {
-  alias less='bat'
-  # pagingを無効
-  alias cat='bat --paging=never'
-}
-zinit wait lucid light-mode as'program' from'gh-r' for \
+zinit lucid light-mode as'program' from'gh-r' for \
   pick'bat*/bat' \
-  atload'__bat_atload' \
   @'sharkdp/bat'
+alias less='bat'
+# pagingを無効
+alias cat='bat --paging=never'
 
 ### lsd ###
-__lsd_atload() {
-  # ファイル→ディレクトの順でほぼ全て表示（.と..は除く）
-  alias ls='lsd -A --group-dirs=last'
-  # サイズが人に優しいリスト表示で、ディレクトリのサイズは中のファイルの合計を表示
-  alias ll='lsd -Ahl --total-size --group-dirs=last'
-  # ツリー表示
-  alias tree='lsd -A --tree --group-dirs=last'
-  # ツリー形式でファイル情報も表示
-  alias lr='lsd -Ahl --total-size --tree --group-dirs=last'
-}
-zinit wait lucid light-mode as'program' from'gh-r' for \
+zinit lucid light-mode as'program' from'gh-r' for \
   pick'lsd*/lsd' \
-  atload'__lsd_atload' \
   @'Peltoche/lsd'
+# ファイル→ディレクトの順でほぼ全て表示（.と..は除く）
+alias ls='lsd -A --group-dirs=last'
+# サイズが人に優しいリスト表示で、ディレクトリのサイズは中のファイルの合計を表示
+alias ll='lsd -Ahl --total-size --group-dirs=last'
+# ツリー表示
+alias tree='lsd -A --tree --group-dirs=last'
+# ツリー形式でファイル情報も表示
+alias lr='lsd -Ahl --total-size --tree --group-dirs=last'
 
 ### navi ###
 __navi_search() {
   LBUFFER="$(navi --print --query="$LBUFFER")"
   zle reset-prompt
 }
-__navi_atload() {
-  export NAVI_PATH="$XDG_DATA_HOME/navi/"
-  export NAVI_CONFIG="$XDG_CONFIG_HOME/navi/config.yaml"
-
-  zle -N __navi_search
-  bindkey '^N' __navi_search
-}
-zinit wait lucid light-mode as'program' from'gh-r' bpick'*apple-darwin*' for \
-  atload'__navi_atload' \
+export NAVI_PATH="$XDG_DATA_HOME/navi/"
+export NAVI_CONFIG="$XDG_CONFIG_HOME/navi/config.yaml"
+zinit lucid light-mode as'program' from'gh-r' bpick'*apple-darwin*' for \
   @'denisidoro/navi'
+zle -N __navi_search
+bindkey '^N' __navi_search
 
 ### custom widgets ###
 source "$ZDOTDIR/.widgets.zsh"
@@ -89,7 +72,7 @@ bindkey '^R' __fzf_history_selection
 bindkey '^G' __fzf_ghq_cd
 
 ### zsh plugins ###
-zinit wait lucid blockf light-mode for \
+zinit lucid blockf light-mode for \
   @'zsh-users/zsh-completions' \
   @'zdharma-continuum/fast-syntax-highlighting' \
 
